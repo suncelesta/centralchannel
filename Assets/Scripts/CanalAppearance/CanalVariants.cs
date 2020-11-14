021caaf634a2ab6c3632c633500a5a9604dfd108
suncelesta <suncelesta@gmail.com>
2020-11-14 20:02:34 +0300

    Toogle between male and female

---
+++ Assets/Scripts/CanalAppearance/CanalVariants.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanalVariants : MonoBehaviour
+{
+    public SideCanal left;
+    public SideCanal right;
+
+    public void MakeMale(bool doMake)
+    {
+        if (doMake)
+        {
+            left.MakeRed();
+            right.MakeWhite();
+        }
+    }
+    
+    public void MakeFemale(bool doMake)
+    {
+        if (doMake)
+        {
+            left.MakeWhite();
+            right.MakeRed();
+        }
+    }
+}
---
+++ Assets/Scripts/CanalAppearance/SideCanal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SideCanal : MonoBehaviour
+{
+    public Material[] redMaterials;
+    public Material[] whiteMaterials;
+
+    public void MakeRed()
+    {
+        SetMaterials(redMaterials);
+    }
+
+    public void MakeWhite()
+    {
+        SetMaterials(whiteMaterials);
+    }
+    
+    private void SetMaterials(Material[] materials)
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.materials = materials;
+    }
+}
